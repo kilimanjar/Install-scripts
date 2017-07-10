173d6a552c12f763db691a1c0fc417d0da30fc83
kilimanjar <kilimanjar@users.noreply.github.com>
2017-07-10 19:09:42 +0200

    Create centos.start.sh

---
+++ centos.start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+yum remove -y dhclient
+yum update
+yum install nano curl wget bash-completion setools-console policycoreutils-python iptables-services epel-release
+
+systemctl disable firewalld
+systemctl enable iptables
+systemctl start iptables
+systemctl enable ip6tables
+systemctl start ip6tables
+
+exit 0
