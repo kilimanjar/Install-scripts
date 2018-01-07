fbeee6528b2f2581ebcf2b0a9740a9b9a1066873
kilimanjar <kilimanjar@users.noreply.github.com>
2018-01-07 18:48:55 +0100

    Update common-centos.sh

--- common-centos.sh
+++ common-centos.sh
@@ -3,12 +3,12 @@
 echo "alias ls='ls -la --color=auto --group-directories-first'" >> /etc/skel/.bashrc
 echo "alias ls='ls -la --color=auto --group-directories-first'" >> ~/.bashrc
 
-yum autoremove -y dhclient wpa_supplicant
+yum autoremove -y dhclient wpa_supplicant plymouth iwl* ivtv-firmware alsa-firmware aic94xx-firmware
 yum update
 yum install nano curl wget bash-completion setools-console policycoreutils-python iptables-services epel-release
-yum autoremove
 
 systemctl disable firewalld
+yum autoremove -y firewalld
 systemctl enable iptables
 systemctl start iptables
 systemctl enable ip6tables
